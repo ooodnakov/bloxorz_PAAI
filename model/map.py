69b68365f53708adc34663404d9bba6371b84846
rinle <rinle.it@gmail.com>
2018-04-14 11:10:27 +0700

    Loads Map

--- model/map.py
+++ model/map.py
@@ -1,4 +1,14 @@
 """Map"""
+import simplejson as json
 
-class maps:
-    pass
+class maps(object):
+    def __init__(self, link_path_level=None):
+        self.files = None
+        self.jsonObject = None
+        if link_path_level != None:
+            self.loadLevel(link_path_level)
+        # print(self.jsonObject["swO"])
+
+    def loadLevel(self, link_path_level=None):
+        self.files = open(link_path_level, "r")
+        self.jsonObject = json.loads(self.files.read()) 
--- run.py
+++ run.py
@@ -1,1 +1,5 @@
 """Run"""
+from model.map import maps
+
+if __name__=="__main__":
+    test =  maps("./level/0.json")
